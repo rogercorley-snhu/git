72d4744f4b53255b20a1191cb72b0fb546104c66
Roger Corley <roger.corley@snhu.edu>
2015-03-30 15:57:54 -0500

    Balance Reset Queries - FINAL

--- SQL_Queries/sql_ResetBalances_SEMO.sql
+++ SQL_Queries/sql_ResetBalances_SEMO.sql
@@ -1,70 +1,76 @@
-/*		SEMO BALANCE RESET QUERIES		*/
+/*		BALANCE RESET QUERIES		*/
 
-USE GEMdb
+USE [GEMdb];
 GO
 
-DECLARE	@__TDATE datetime, @__OUTNO int, @__TID int, @__REFNO varchar(4), @__CHKNO varchar(6); 
-DECLARE @__BEGINDATE datetime, @__ENDDATE datetime;
-DECLARE @__BADGENO varchar(19);
+DECLARE		@_TDATE datetime, @_BEGINDATE datetime, @_ENDDATE datetime;
+DECLARE		@_CID int, @_OUTNO int, @_TID int; 
+DECLARE		@_BID char(8), @_REFNO char(4), @_CHKNO char(6); 
+DECLARE		@_BadgeNo char(19);
 
 
-SET		@__TDATE = '03-21-2015 23:59:00';
+/*	---------------------------------------------------------------------------	
+		[	 ***	MODIFY THE FOLLOWING VALUES ONLY	*** 	]
+-------------------------------------------------------------------------------*/
+
+SET			@_TDATE			= '03-26-2015 23:59:59';
+
+SET			@_BID			= 'Payments';
+SET			@_CID			= 1;
+SET			@_OUTNO			= 1000;
+SET			@_TID			= 501;
+SET			@_REFNO			= 'AUTO';
+SET			@_CHKNO			= 'BIWK';
+SET			@_BadgeNo		= NULL;
+
+-------------------------------------------------------------------------------
 
 
-SET		@__OUTNO =	1000;
-SET		@__TID =	501;
-SET		@__REFNO = 'AUTO';
-SET		@__CHKNO = 'BIWK';
 
+SET			@_BEGINDATE		= (SELECT BeginDate FROM tblCycleXlat AS xlat WHERE @_TDATE BETWEEN xlat.BeginDate AND xlat.EndDate AND xlat.xlatID = @_CHKNO);
+SET			@_ENDDATE		= (SELECT EndDate FROM tblCycleXlat AS xlat WHERE @_TDATE BETWEEN xlat.BeginDate AND xlat.EndDate AND xlat.xlatID = @_CHKNO);
 
-SET		@__BEGINDATE =	(SELECT BeginDate FROM tblCycleXlat AS xlat WHERE @__TDATE BETWEEN xlat.BeginDate AND xlat.EndDate AND xlat.xlatID = @__CHKNO);
-SET		@__ENDDATE =	(SELECT EndDate FROM tblCycleXlat AS xlat WHERE @__TDATE BETWEEN xlat.BeginDate AND xlat.EndDate AND xlat.xlatID = @__CHKNO);
-
---SET		@__BADGENO =	(SELECT b.badgeno from tblBadgesOHD as b left join tblDetail as d on d.accountno = b.accountno join tblAccountOHD as a on d.AccountNo = a.AccountNo where b.AccountNo = a.AccountNo); 
-
-
-SELECT	'Payments' AS BatchID
-		,'1' AS CoreID
-		,ohd.AccountNo AS AccountNo
-
-		,@__TDATE AS TransDate
-		,@__OUTNO AS OutletNo
-		,@__TID AS TransID
-		,@__REFNO AS RefNum
-		,@__CHKNO AS ChkNum
-		
-		
-		,CASE WHEN SUM(dtl.TransTotal) < 0 THEN 0 ELSE SUM(dtl.TransTotal) END AS TransTotal
-		,CASE WHEN SUM(dtl.TransTotal) < 0 THEN 0 ELSE SUM(dtl.TransTotal) END AS Sales1
-		
-		,@__BADGENO AS BadgeNo
-		
-INTO	RESETS
-
-FROM	tblDetail AS dtl
-		LEFT JOIN tblAccountOHD AS ohd ON dtl.AccountNo = ohd.AccountNo
-
-WHERE	--dtl.TransID IN (1,2,3) AND 
-		ohd.AccountClassID IN (10) AND
-		dtl.TransDate BETWEEN @__BEGINDATE AND @__ENDDATE AND
-		dtl.AccountNo NOT LIKE 1234
+SELECT		 @_BID AS BatchID
+			,@_CID AS CoreID
+			,ohd.AccountNo AS AccountNo
+			,@_TDATE AS TransDate
+			,@_OUTNO AS OutletNo
+			,@_TID AS TransID
+			,@_REFNO AS RefNum
+			,@_CHKNO AS ChkNum
+			
+			
+			,CASE WHEN SUM(dtl.TransTotal) < 0 THEN 0 ELSE SUM(dtl.TransTotal) END AS TransTotal
+			,CASE WHEN SUM(dtl.TransTotal) < 0 THEN 0 ELSE SUM(dtl.TransTotal) END AS Sales1
+			
+			,@_BADGENO AS BadgeNo
 
 		
-GROUP BY ohd.AccountNo
+INTO		RESETS
 
-HAVING SUM(dtl.TransTotal) <> 0
+FROM		tblDetail AS dtl
+				LEFT JOIN tblAccountOHD AS ohd ON dtl.AccountNo = ohd.AccountNo
+		
+WHERE		dtl.TransID IN (1,2,4,15) AND 
+			ohd.AccountClassID IN (10,40) AND
+			dtl.TransDate BETWEEN @_BEGINDATE AND @_ENDDATE
 
-ORDER BY dbo.LPad(RTRIM(ohd.AccountNo),19,'0')
+GROUP BY 	ohd.AccountNo
+
+HAVING 		SUM(dtl.TransTotal) <> 0
+
+ORDER BY 	dbo.LPad(RTRIM(ohd.AccountNo),19,'0')
 
 
 
-USE GEMdb
+
+USE [GEMdb];
 GO
 
-UPDATE dbo.RESETS
+UPDATE 		dbo.RESETS
 
-SET BadgeNo = d.badgeno
+SET			BadgeNo = d.BadgeNo
 
-from (select a.accountno, b.badgeno from tblAccountOHD as a join tblBadgesOHD as b on a.AccountNo = b.AccountNo) as d, RESETS as c
+FROM 		(SELECT a.AccountNo, b.BadgeNo FROM tblAccountOHD AS a join tblBadgesOHD AS b on a.AccountNo = b.AccountNo) AS d, RESETS AS c
 
-where c.accountno = d.accountno
+WHERE 		c.AccountNo = d.AccountNo
