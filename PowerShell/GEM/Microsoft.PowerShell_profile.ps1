763a12ab71a69319247f4734793fdeb493e2d917
Roger Corley <roger.corley@snhu.edu>
2015-05-13 18:20:18 -0500

    Yet another Win Change

--- PowerShell/GEM/Microsoft.PowerShell_profile.ps1
+++ PowerShell/GEM/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,35 @@
-Set-Location C:\_gemToolbox\PowerShell\
+Set-Location C:\PowerShell
 
-. C:\_gemToolbox\PowerShell\gemToolbox.ps1
+$Shell = $Host.UI.RawUI
+$size = $Shell.WindowSize
+$size.width=100
+$size.height=60
+$Shell.WindowSize = $size
+$size = $Shell.BufferSize
+$size.width=1024
+$size.height=5000
+$Shell.BufferSize = $size
+
+$Shell.BackgroundColor = "Black"
+$Shell.ForegroundColor = "White"
+
+$Shell.WindowTitle="Pinky & the Brain"
+$Shell.WindowTitle="Pinky & the Brain"
+
+New-Item alias:np -Value C:\Windows\System32\notepad.exe
+
+. C:\PowerShell\Functions\Tools.ps1
 
 Clear-Host
+
+function Prompt
+  { 
+  $promptSpace = "  "
+  $promptLine = "________________________________________________________________________________" 
+    $promptString = "--[  " + $(Get-Location) + "  ]----|"
+  Write-Host $promptSpace
+  Write-Host $promptLine -ForegroundColor Yellow
+  Write-Host $promptSpace
+  Write-Host $promptString -NoNewLine -ForegroundColor Yellow
+    Return " " 
+  }
