33f19229a37b72fa9e0a34db879441f37afe8159
Roger Corley <roger.corley@snhu.edu>
2015-10-30 14:54:28 -0500

    F

--- SQL_Queries/SQL Tools/sql_FindDuplicates.sql
+++ SQL_Queries/SQL Tools/sql_FindDuplicates.sql
@@ -1,15 +1,24 @@
-SELECT
-	 i.< COLUMN >
-	,c.dupeCount
+DECLARE @table varchar(max), @sql nvarchar(max)
+DECLARE @col1 varchar(max), @col2 varchar(max), @col3 varchar(max)
 
-FROM
-	< TABLE > AS i
+SET @col1 = 'AccountNo'
+SET @col2 = 'LastName'
+SET @col3 = 'FirstName'
 
-INNER JOIN (
+SET @table = 'tblAccountImport'
 
-		SELECT		< SAME COLUMN AS ABOVE >, COUNT(*) AS dupeCount
-		FROM		< SAME TABLE AS ABOVE >
-		GROUP BY	< SAME COLUMN AS ABOVE >
-		HAVING COUNT(*) > 1
 
-	) c ON i.< SAME COLUMN AS ABOVE > = c.< SAME COLUMN AS ABOVE >
+SET @sql = '
+ SELECT i.'+@col1+', i.'+@col2+', i.'+@col3+'
+ FROM ' +@table+ ' AS i
+ INNER JOIN (
+  SELECT '+@col1+', COUNT(*) AS dupeCount
+  FROM ' +@table+ '
+  GROUP BY '+@col1+'
+  HAVING COUNT(*) > 1
+ ) AS c ON i.'+@col1+' = c.'+@col1+'
+
+ ORDER BY '+@col1+'
+ '
+
+exec sp_executesql @sql
